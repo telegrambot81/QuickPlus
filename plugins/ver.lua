cdea35e74f8049792e7a1713c72871d8bb9508a3
telegrambot81 <telegrambot81@gmail.com>
2016-04-04 20:26:51 +0430

    Update ver.lua

--- plugins/ver.lua
+++ plugins/ver.lua
@@ -1,24 +1,12 @@
 do
 
 function run(msg, matches)
-  return 'QuickPLUS'.. VERSION .. [[ 
+  return 'kingbot'.. VERSION .. [[ 
  
-  Developer : @AmirQuick
-  Manager  : @EhsanQuick
-  Manager  : @SAmyarQuick
-  Manager  : @AmirAliQuick
-  Sponsor : #Ehsan & #SAMYAR
   
-I thank my friend
-#amir_h
-#minaco
-#reza
-#nova
 And many more..
 
-Our Chanel: https://telegram.me/QuickGuardTeam
 Profile Robot
-@QuickPLUS
 VERSION 1
 #date of birth 1394.12.20
 #Doomed to be always online  ]]
